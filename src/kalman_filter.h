e478256801f126d2bebe6a3841781406a375c0f5
Gergő <46539838+gergo-szabo@users.noreply.github.com>
2019-02-19 08:53:06 -0800

    Update kalman_filter.h
    Moving radar and laser R and H matrices into the KF class.
    Tools included because of Jacobian function.

--- src/kalman_filter.h
+++ src/kalman_filter.h
@@ -2,9 +2,40 @@
 #define KALMAN_FILTER_H_
 
 #include "Eigen/Dense"
+#include "tools.h"
 
 class KalmanFilter {
- public:
+  Tools tools;
+ 
+  public:
+ 
+  // state vector
+  Eigen::VectorXd x_;
+
+  // state covariance matrix
+  Eigen::MatrixXd P_;
+
+  // state transition matrix
+  Eigen::MatrixXd F_;
+
+  // process covariance matrix
+  Eigen::MatrixXd Q_;
+
+  // measurement matrix, normal KF
+  Eigen::MatrixXd H_;
+ 
+  // measurement matrix, extended KF
+  Eigen::MatrixXd Hj_;
+
+  // measurement covariance matrix, laser
+  Eigen::MatrixXd R_r;
+  
+  // measurement covariance matrix, radar
+  Eigen::MatrixXd R_l;
+ 
+  // Identity matrix
+  Eigen::MatrixXd I_; 
+ 
   /**
    * Constructor
    */
@@ -17,16 +48,10 @@ class KalmanFilter {
 
   /**
    * Init Initializes Kalman filter
-   * @param x_in Initial state
-   * @param P_in Initial state covariance
-   * @param F_in Transition matrix
-   * @param H_in Measurement matrix
-   * @param R_in Measurement covariance matrix
-   * @param Q_in Process covariance matrix
-   * @const Identity matrix
    */
   void Init(Eigen::VectorXd &x_in, Eigen::MatrixXd &P_in, Eigen::MatrixXd &F_in,
-            Eigen::MatrixXd &H_in, Eigen::MatrixXd &R_in, Eigen::MatrixXd &Q_in);
+            Eigen::MatrixXd &H_in, Eigen::MatrixXd &Hj_in, Eigen::MatrixXd &R_l_in,
+            Eigen::MatrixXd &R_r_in, Eigen::MatrixXd &Q_in);
 
   /**
    * Prediction Predicts the state and the state covariance
@@ -46,27 +71,6 @@ class KalmanFilter {
    * @param z The measurement at k+1
    */
   void UpdateEKF(const Eigen::VectorXd &z);
-
-  // state vector
-  Eigen::VectorXd x_;
-
-  // state covariance matrix
-  Eigen::MatrixXd P_;
-
-  // state transition matrix
-  Eigen::MatrixXd F_;
-
-  // process covariance matrix
-  Eigen::MatrixXd Q_;
-
-  // measurement matrix
-  Eigen::MatrixXd H_;
-
-  // measurement covariance matrix
-  Eigen::MatrixXd R_;
- 
-  // Identity matrix
-  Eigen::MatrixXd I_; 
 };
 
 #endif // KALMAN_FILTER_H_
